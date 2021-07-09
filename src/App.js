cffda5c66bf65cec575db2670ac48188b206e337
UncleGabi <raboczki.gabor92@gmail.com>
2021-07-09 16:25:17 +0200

    Testing if pre-commit tests work - format & eslint check

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import "./App.scss";
 
 const App = ()=> {
     return (
-                            <div className="App">
-                                <header className="App-header">
-                                    <img
-                                        src="https://i.pinimg.com/originals/71/fa/7b/71fa7ba371469361f612b8e5fc9ef80d.png"
-                                        style={{ height: "400px" }}
-                                        alt="logo" />
-                                </header>
-                            </div>
+        <div className="App">
+            <header className="App-header">
+                <img
+                    src="https://i.pinimg.com/originals/71/fa/7b/71fa7ba371469361f612b8e5fc9ef80d.png"
+                    style={{ height: "400px" }}
+                    alt="logo" />
+            </header>
+        </div>
     );
 };
 
