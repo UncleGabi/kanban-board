11b303c1b486aee971740fcb34e24107d9ff78ad
Gabor Raboczki1 <gabor_raboczki1@epam.com>
2021-07-09 15:42:55 +0200

    addig eslint and prettier as tools and git hooks format our code before commiting it to git

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img
-          src="https://i.pinimg.com/originals/71/fa/7b/71fa7ba371469361f612b8e5fc9ef80d.png"
-          style={{ height: "400px" }}
-          alt="logo"
-        />
-      </header>
-    </div>
-  );
-}
+const App = ()=> {
+    return (
+                            <div className="App">
+                                <header className="App-header">
+                                    <img
+                                        src="https://i.pinimg.com/originals/71/fa/7b/71fa7ba371469361f612b8e5fc9ef80d.png"
+                                        style={{ height: "400px" }}
+                                        alt="logo" />
+                                </header>
+                            </div>
+    );
+};
 
 export default App;
