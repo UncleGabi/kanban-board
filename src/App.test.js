0cd9a431628c3165fe166eaf893a2430889c703f
UncleGabi <raboczki.gabor92@gmail.com>
2021-07-09 17:26:15 +0200

    One approval is needed for a commit and tests are run before a commit via husky

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import App from "./App";
 test("renders learn react link", () => {
 
     render(<App />);
-    const linkElement = screen.getByText(/Soon.../i);
+    const linkElement = screen.getByText(/Soon../i);
     expect(linkElement).toBeInTheDocument();
 
 });
