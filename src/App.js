549a588c32104889b892d416209cdfc383036b02
UncleGabi <raboczki.gabor92@gmail.com>
2021-07-12 14:14:31 +0200

    Addig GitHub workflow to my repository

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const App = () => {
     return (
         <div className="App">
             <header className="App-header">
-                <h1>soon..</h1>
+                <h1>Soon...</h1>
                 <img
                     src="https://i.pinimg.com/originals/71/fa/7b/71fa7ba371469361f612b8e5fc9ef80d.png"
                     style={{ height: "400px" }}
