0a60b8a558ddb9d3b4a5c6967c651159e9491406
UncleGabi <raboczki.gabor92@gmail.com>
2021-07-13 13:35:28 +0200

    Added an extra div to the Home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,8 @@ const Home = () => {
             className="home"
             style={{ textAlign: "center", marginTop: "200px" }}
         >
-            <h1>Welcome to my Kanban App!</h1>
+            <h1>Welcome to my Kanban App</h1>
+            <div>This is the beginning of a beautiful friendship!</div>
         </div>
     );
 };
