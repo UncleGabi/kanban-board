65a98bc9ba2036ba8e41b6c7e5bded5450a357a7
UncleGabi <raboczki.gabor92@gmail.com>
2021-07-13 17:26:32 +0200

    creating the navbar of the page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,12 +6,6 @@ const Home = () => {
         >
             <div>
                 <h1>Welcome to my Kanban App!</h1>
-                <p>Will this work?</p>
-            </div>
-
-            <div>
-                <h1>Welcome to my Kanban App</h1>
-                <div>This is the beginning of a beautiful friendship!</div>
             </div>
         </div>
     );
